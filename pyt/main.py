3e3502af4442530365d22d5e9fbc3028c0347822
YusufKhamdamov <88310221+YusufKhamdamov@users.noreply.github.com>
2021-08-02 12:30:28 +0500

    Add files via upload

---
+++ pyt/main.py
@@ -0,0 +1,39 @@
+# bolen malumot turi
+
+
+# oddiySon = int(input("sonning qandayligini koring"))
+
+# if oddiySon  ==0:
+#     print("A soni nolga ten")
+# elif oddiySon % 2 ==0:
+#  print("A juft son")
+# else:
+#     print("A toq san")
+
+
+
+# logichskiy operatorlar
+#and = && - va
+#or - || -yoki -ili
+
+# A = int(input("sonning qandayligini koring"))
+# B = int(input("sonning qandayligini koring"))
+
+
+# if A >2  and B<=3:
+#  print("togri")
+# else:
+#  print("notogri")
+
+
+# A = int(input("sonning qandayligini koring"))
+# B = int(input("sonning qandayligini koring"))
+# C = int(input("sonning qandayligini koring"))
+# if A<=B and B<=C:
+#     print("rost")
+# else:
+#     print("rost emas")
+
+A = int(input("sonning qandayligini koring"))
+B = int(input("sonning qandayligini koring"))
+
